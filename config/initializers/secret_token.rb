f289880122c9750499bce16e8ac96ee6935ef22a
Ahmed Hassan <ah070413@gmail.com>
2013-09-13 00:03:25 +0100

    initial setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ponona::Application.config.secret_key_base = 'b97655370f94fd8ccd6744597beb42c2bcc2d9622358641c35f6cd00395cb868dcea8c8c4acfa2b67c7eada5e3791d936e84778ac4349e90ebd7052c7e63dc8d'
